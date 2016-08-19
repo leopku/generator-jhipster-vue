9188a10238537c36579335d862defeab63dc9cf9
leopku <leopku@qq.com>
2016-08-19 16:00:19 +0800

    initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# generator-jhipster-vue
+[![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> JHipster module, Generates vue&amp;vuex based app for JHipster-spring boot based-application
+
+# Introduction
+
+This is a [JHipster](http://jhipster.github.io/) module, that is meant to be used in a JHipster application.
+
+# Prerequisites
+
+As this is a [JHipster](http://jhipster.github.io/) module, we expect you have JHipster and its related tools already installed:
+
+- [Installing JHipster](https://jhipster.github.io/installation.html)
+
+# Installation
+
+To install this module:
+
+```bash
+npm install -g generator-jhipster-vue
+```
+
+To update this module:
+```bash
+npm update -g generator-jhipster-vue
+```
+
+# Usage
+
+# License
+
+Apache-2.0 © [Song Liu]
+
+[npm-image]: https://img.shields.io/npm/v/generator-jhipster-vue.svg
+[npm-url]: https://npmjs.org/package/generator-jhipster-vue
+[travis-image]: https://travis-ci.org/leopku/generator-jhipster-vue.svg?branch=master
+[travis-url]: https://travis-ci.org/leopku/generator-jhipster-vue
+[daviddm-image]: https://david-dm.org/leopku/generator-jhipster-vue.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/leopku/generator-jhipster-module
---
+++ generators/app/index.js
@@ -0,0 +1,89 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var packagejs = require(__dirname + '/../../package.json');
+
+// Stores JHipster variables
+var jhipsterVar = {moduleName: 'vue'};
+
+// Stores JHipster functions
+var jhipsterFunc = {};
+
+module.exports = yeoman.Base.extend({
+
+  initializing: {
+    compose: function (args) {
+      this.composeWith('jhipster:modules',
+        {
+          options: {
+            jhipsterVar: jhipsterVar,
+            jhipsterFunc: jhipsterFunc
+          }
+        },
+        this.options.testmode ? {local: require.resolve('generator-jhipster/modules')} : null
+      );
+    },
+    displayLogo: function () {
+      // Have Yeoman greet the user.
+      this.log('Welcome to the ' + chalk.red('JHipster vue') + ' generator! ' + chalk.yellow('v' + packagejs.version + '\n'));
+    }
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    var prompts = [{
+      type: 'input',
+      name: 'message',
+      message: 'Please put something',
+      default: 'hello world!'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      // To access props later use this.props.someOption;
+
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    writeTemplates : function () {
+      this.baseName = jhipsterVar.baseName;
+      this.packageName = jhipsterVar.packageName;
+      this.angularAppName = jhipsterVar.angularAppName;
+      var javaDir = jhipsterVar.javaDir;
+      var resourceDir = jhipsterVar.resourceDir;
+      var webappDir = jhipsterVar.webappDir;
+
+      this.message = this.props.message;
+
+      this.log('baseName=' + this.baseName);
+      this.log('packageName=' + this.packageName);
+      this.log('angularAppName=' + this.angularAppName);
+      this.log('message=' + this.message);
+
+      this.template('_dummy.txt', 'dummy.txt', this, {});
+    },
+
+    writeCommonFiles: function () {
+      this.template('_package.json', 'package1.json');
+    },
+
+    registering: function () {
+      try {
+        jhipsterFunc.registerModule("generator-jhipster-vue", "entity", "post", "app", "Generates vue&amp;vuex based app for JHipster-spring boot based-application");
+      } catch (err) {
+        this.log(chalk.red.bold('WARN!') + ' Could not register as a jhipster entity post creation hook...\n');
+      }
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  },
+
+  end: function () {
+    this.log('End of vue generator');
+  }
+});
